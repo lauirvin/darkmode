2a30196fa6136ae164d04c83e0ce01ec66172f3e
lauirvin <lauirvin98@gmail.com>
2019-10-15 22:36:04 +0800

    More styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Fragment } from "react";
 import "./styles/styles.css";
 
-import GitHub from "./media/github.svg";
-import Instagram from "./media/instagram.svg";
-import LinkedIn from "./media/linkedin.svg";
-import YouTube from "./media/youtube.svg";
+import GitHub from "./media/socials/github.svg";
+import Instagram from "./media/socials/instagram.svg";
+import LinkedIn from "./media/socials/linkedin.svg";
+import YouTube from "./media/socials/youtube.svg";
 
 function App() {
   return (
@@ -16,18 +16,34 @@ function App() {
         Developed by <span>Irvin Ives Lau</span>
       </h2>
       <div className="socials">
-        <img src={GitHub} />
-        <img src={Instagram} />
-        <img src={LinkedIn} />
-        <img src={YouTube} />
+        <a target="_blank" href="https://github.com/lauirvin">
+          <img alt="github" src={GitHub} />
+        </a>
+        <a target="_blank" href="https://instagram.com/irvin.dev">
+          <img alt="instagram" src={Instagram} />
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/in/lauirvin">
+          <img alt="linkedin" src={LinkedIn} />
+        </a>
+        <a target="_blank" href="https://youtube.com/lauirvin98">
+          <img alt="youtube" src={YouTube} />
+        </a>
       </div>
       <div className="links">
-        <button className="view-source">
+        <a
+          target="_blank"
+          href="https://github.com/lauirvin/darkmode"
+          className="view-source"
+        >
           <p>View Source</p>
-        </button>
-        <button className="about-author">
+        </a>
+        <a
+          target="_blank"
+          href="https://irviniveslau.com"
+          className="about-author"
+        >
           <p>About Author</p>
-        </button>
+        </a>
       </div>
     </Fragment>
   );
