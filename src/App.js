ccf907958a88600e1f408c4c9d06b8be4544d99a
lauirvin <lauirvin98@gmail.com>
2019-10-15 22:05:00 +0800

    Initial layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./styles/styles.css";
+
+import GitHub from "./media/github.svg";
+import Instagram from "./media/instagram.svg";
+import LinkedIn from "./media/linkedin.svg";
+import YouTube from "./media/youtube.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>
+        <u>“Light Mode”</u>
+      </h1>
+      <h2>
+        Developed by <span>Irvin Ives Lau</span>
+      </h2>
+      <div className="socials">
+        <img src={GitHub} />
+        <img src={Instagram} />
+        <img src={LinkedIn} />
+        <img src={YouTube} />
+      </div>
+      <div className="links">
+        <button className="view-source">
+          <p>View Source</p>
+        </button>
+        <button className="about-author">
+          <p>About Author</p>
+        </button>
+      </div>
+    </Fragment>
   );
 }
 
