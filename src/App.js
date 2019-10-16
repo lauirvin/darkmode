b8eb56f5ebf28682b48b439bab56ce24db4f195d
lauirvin <lauirvin98@gmail.com>
2019-10-16 22:44:25 +0800

    Local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import "./styles/styles.css";
 
 import GitHub from "./media/socials/github.svg";
@@ -6,32 +6,68 @@ import Instagram from "./media/socials/instagram.svg";
 import LinkedIn from "./media/socials/linkedin.svg";
 import YouTube from "./media/socials/youtube.svg";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    const localMode = localStorage.getItem("mode");
+    const body = document.body.classList;
+
+    localMode === "mod-dark" ? body.add("mod-dark") : body.remove("mod-dark");
+  }, []);
+
+  const handleSwitch = () => {
+    const localMode = localStorage.getItem("mode");
+    const body = document.body.classList;
+
+    localMode === "mod-dark" ? body.remove("mod-dark") : body.add("mod-dark");
+
+    if (localMode === "mod-dark") {
+      localStorage.setItem("mode", "");
+    } else {
+      localStorage.setItem("mode", "mod-dark");
+    }
+  };
+
   return (
     <Fragment>
-      <h1>
-        <u>“Light Mode”</u>
-      </h1>
+      <button onClick={handleSwitch} className="switch"></button>
+      <h1 className="mode"> </h1>
       <h2>
         Developed by <span>Irvin Ives Lau</span>
       </h2>
       <div className="socials">
-        <a target="_blank" href="https://github.com/lauirvin">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/lauirvin"
+        >
           <img alt="github" src={GitHub} />
         </a>
-        <a target="_blank" href="https://instagram.com/irvin.dev">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://instagram.com/irvin.dev"
+        >
           <img alt="instagram" src={Instagram} />
         </a>
-        <a target="_blank" href="https://www.linkedin.com/in/lauirvin">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/lauirvin"
+        >
           <img alt="linkedin" src={LinkedIn} />
         </a>
-        <a target="_blank" href="https://youtube.com/lauirvin98">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://youtube.com/lauirvin98"
+        >
           <img alt="youtube" src={YouTube} />
         </a>
       </div>
       <div className="links">
         <a
           target="_blank"
+          rel="noopener noreferrer"
           href="https://github.com/lauirvin/darkmode"
           className="view-source"
         >
@@ -39,6 +75,7 @@ function App() {
         </a>
         <a
           target="_blank"
+          rel="noopener noreferrer"
           href="https://irviniveslau.com"
           className="about-author"
         >
@@ -47,6 +84,6 @@ function App() {
       </div>
     </Fragment>
   );
-}
+};
 
 export default App;
